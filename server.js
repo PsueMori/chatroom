2860afb974f02144187932a14d3af5549d91734d
PsueMori <branferguson612@gmail.com>
2026-03-09 09:48:06 -0500

    Update server.js

--- server.js
+++ server.js
@@ -2,29 +2,46 @@ const express = require("express");
 const app = express();
 const http = require("http").createServer(app);
 const { Server } = require("socket.io");
+const multer = require("multer");
+const path = require("path");
 
 const io = new Server(http);
 
+// store users
+let users = {};
+
+// file upload setup
+const storage = multer.diskStorage({
+  destination: "./public/uploads/",
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + path.extname(file.originalname));
+  }
+});
+
+const upload = multer({ storage });
+
+// serve frontend
 app.use(express.static("public"));
 
-let users = {}; // store connected users
+// upload endpoint
+app.post("/upload", upload.single("file"), (req, res) => {
+  res.json({
+    file: "/uploads/" + req.file.filename,
+    name: req.file.originalname
+  });
+});
 
+// socket logic
 io.on("connection", (socket) => {
-  console.log("User connected");
 
-  // when a user sets a username
   socket.on("set username", (username) => {
     socket.username = username;
     users[socket.id] = username;
 
-    // broadcast user joined
     io.emit("user joined", username);
-
-    // update user list for everyone
     io.emit("update users", Object.values(users));
   });
 
-  // handle messages
   socket.on("chat message", (msg) => {
     io.emit("chat message", {
       user: socket.username || "Anonymous",
@@ -32,22 +49,26 @@ io.on("connection", (socket) => {
     });
   });
 
-  // handle disconnect
+  socket.on("file message", (data) => {
+    io.emit("file message", {
+      user: socket.username,
+      file: data.file,
+      name: data.name
+    });
+  });
+
   socket.on("disconnect", () => {
     if (socket.username) {
-      console.log(`${socket.username} disconnected`);
-
-      // broadcast user left
       io.emit("user left", socket.username);
-
-      // remove from users list
       delete users[socket.id];
       io.emit("update users", Object.values(users));
     }
   });
+
 });
 
 const PORT = process.env.PORT || 3000;
+
 http.listen(PORT, () => {
   console.log("Server running on port " + PORT);
 });
